1ed811bbde2b8c7d36689ad1d96d84286a251f3c
Caity <caitykondo@gmail.com>
2017-04-08 15:59:47 -1000

    add redux Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './containers/App';
 import './index.css';
 
+let store = createStore();
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
