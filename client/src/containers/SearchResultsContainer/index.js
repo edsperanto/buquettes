732c614247249462abefcb2ed949572bdc0aeee1
Caity <caitykondo@gmail.com>
2017-04-13 20:50:24 -1000

    Remove case-sensitive search results

--- client/src/containers/SearchResultsContainer/index.js
+++ client/src/containers/SearchResultsContainer/index.js
@@ -12,7 +12,7 @@ class SearchResultsContainer extends Component {
       <div className="search-results">
         {
           this.props.files.files.filter( file => {
-            return file.name.indexOf(this.props.query) !== -1;
+            return file.name.toLowerCase().indexOf(this.props.query.toLowerCase()) !== -1;
           }).map( file => {
             return (
               <File
