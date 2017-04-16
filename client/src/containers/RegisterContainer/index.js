6e285328f53703cda06ef925d0592074da299fa9
Caity <caitykondo@gmail.com>
2017-04-15 15:57:19 -1000

    Form inputs can store values in component state

--- client/src/containers/RegisterContainer/index.js
+++ client/src/containers/RegisterContainer/index.js
@@ -3,7 +3,21 @@ import React, { Component } from 'react';
 class RegisterContainer extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+      email: '',
+      first_name: '',
+      last_name: '',
+      password: ''
+    }
+  }
 
+  handleChange = (event) => {
+    this.setState(
+      {
+        [event.target.name]: event.target.value
+      }
+    )
   }
 
   render() {
@@ -11,19 +25,44 @@ class RegisterContainer extends Component {
       <div className="register">
         <form action="#">
           <label for="username">Username</label>
-          <input type="text" name="username" id="username"/>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            onChange={this.handleChange}
+          />
 
           <label for="email">Email</label>
-          <input type="text" name="email" id="email"/>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            onChange={this.handleChange}
+          />
 
           <label for="first_name">First Name</label>
-          <input type="text" name="first_name" id="first_name"/>
+          <input
+            type="text"
+            name="first_name"
+            id="first_name"
+            onChange={this.handleChange}
+          />
 
           <label for="last_name">Last Name</label>
-          <input type="text" name="last_name" id="last_name"/>
+          <input
+            type="text"
+            name="last_name"
+            id="last_name"
+            onChange={this.handleChange}
+          />
 
           <label for="password">Password</label>
-          <input type="text" name="password" id="password"/>
+          <input
+            type="text"
+            name="password"
+            id="password"
+            onChange={this.handleChange}
+          />
         </form>
       </div>
     );
