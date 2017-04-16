06ca354e2499474abd17370bb6be590014b64aa9
Caity <caitykondo@gmail.com>
2017-04-15 19:33:22 -1000

    Created FormTextInput component

---
+++ client/src/components/FormTextInput.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FormTextInput = ({ type, label, name, id, onChange, state }) => {
+  return (
+    <label>
+      <span className="form-label">{label}</span>
+      <input
+        type={ type === "password" ? "password":"text"}
+        name={name}
+        id={id}
+        onChange={onChange}
+        state={state}
+      />
+    </label>
+  )
+}
+
+export default FormTextInput;
