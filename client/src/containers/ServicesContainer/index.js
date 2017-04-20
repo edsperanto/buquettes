48cf48c54f7b32835bffc42f9e9e2ab71c186773
Caity <caitykondo@gmail.com>
2017-04-19 20:49:17 -1000

    Created ServicesContainer component

---
+++ client/src/containers/ServicesContainer/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class ServicesContainer extends Component {
+
+}
+
+function mapStateToProps(state) {
+
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ServicesContainer);
