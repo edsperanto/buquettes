6f1217143a26b22b744ef6604033e7f3bad455c1
Steven Cable <steven.cable12@gmail.com>
2017-04-23 01:33:52 -1000

    working on formatting github data for front end

--- server/routes/github.js
+++ server/routes/github.js
@@ -1,26 +1,24 @@
 module.exports = (dependencies) => {
 
 	// extract dependencies
-	const {express, request, rp, qs, shallowClone, recursive, helper, GitHubOAuth} = dependencies;
+	const {
+		express, request, rp, qs, recursive, helper, GitHubOAuth, credentials: {github: credentials },
+	} = dependencies;
 	const { isAuthenticated } = helper;
 	const router = express.Router();
 
 	// Environmental Variables
 	const env = require('dotenv').config();
-	const { GH_CLIENT_ID, GH_CLIENT_SECRET } = process.env;
+	const { client_id, client_secret } = credentials;
 
-	let userRepoURL = `https://github.com/login/oauth/authorize?scope=repo&client_id=${GH_CLIENT_ID}`;
+	let userRepoURL = `https://github.com/login/oauth/authorize?scope=repo&client_id=${client_id}`;
 
 
 	// Routes
 	router.get('/authorize', isAuthenticated, (_, res) => res.redirect(userRepoURL));
 
 	router.get('/callback', isAuthenticated, ( req , res ) => {     
-		let body = {
-			client_id: GH_CLIENT_ID, 
-			client_secret: GH_CLIENT_SECRET, 
-			code: req.query.code
-		};
+		let body = { client_id , client_secret, code: req.query.code };
 
 		let userPromise = new Promise( ( resolve , reject ) => {
 			request.post({ 
@@ -82,7 +80,10 @@ module.exports = (dependencies) => {
   	)
   	.then((user) => {
   		githubUsername = user.username;
-	  	let searchURL = `https://api.github.com/search/users?q=${githubUsername}` ;
+  		accessT = user.token;
+  		access = `?access_token=${accessT}`;
+	  	console.log('access token: ', accessT);
+	  	let searchURL = `https://api.github.com/user/repos?page=1&per_page=14&access_token=${accessT}`;
 
 	  	return rp.get(
 				{
@@ -95,39 +96,20 @@ module.exports = (dependencies) => {
 		})
 		.then((body) => {
 			let parsedBody = JSON.parse(body);
-			repoURL = parsedBody.items[0].repos_url;
-
-			return GitHubOAuth.findOne(
-				{
-					where: {
-						user_id: req.user.id
-					}
-				}
-			);
-		})
-		.then((chunk) => {
-			accessT = chunk.token;
-			access = `?access_token=${accessT}`;
-			let repoWithAccess = repoURL.concat(access);
-		
-			return rp.get(
-				{
-					url:	repoWithAccess,
-						headers: {
-							'User-Agent': 'Buquettes'
-						}				
-			 	}
-			);
-		})
-		.then((body) => {									
-			let parsedRepos = JSON.parse(body);
+			//console.log('body of search: ', parsedBody);
 			let count = 0;
 
 			return Promise.all(
-				parsedRepos.map((repo) => {						//grab each repo and store in array
+				parsedBody.map((repo) => {						//grab each repo and store in array
+				// console.log('repos?: ', repo);
 					count++;
+					console.log('count: ', count);
 					let slicedURL = repo.commits_url.split('{')[0];
 					let commitURLWithAccess = slicedURL.concat(access);
+					let usersRepo = repo.owner.login;
+					let repoName = repo.name;
+					console.log('who\'s is this?: ', usersRepo);
+					
 
 					return rp.get(
 						{
@@ -138,10 +120,12 @@ module.exports = (dependencies) => {
 						}
 					)
 					.then( (shaArray) => {
+						//console.log('shaaaaaaaa: ', shaArray)
 						let parsedCommits = JSON.parse(shaArray);
 						let newestCommit = parsedCommits[0];
 						let newestSha = newestCommit.sha;
-						let treeURL = `https://api.github.com/repos/${githubUsername}/${repo.name}/git/trees/${newestSha}?recursive=1&access_token=${accessT}`;
+						//console.log('who\'s is this?: ', usersRepo);
+						let treeURL = `https://api.github.com/repos/${usersRepo}/${repoName}/git/trees/${newestSha}?recursive=1&access_token=${accessT}`;
 						
 						return rp.get(
 							{
@@ -151,15 +135,48 @@ module.exports = (dependencies) => {
 								}
 							}
 						);
-					});
+					})
+					.catch(err =>{
+        		console.log('.then with shaArray: ', err);
+      		});
 				})
 			)
 			// then for Promise.all
 			.then( (arrData) => {
-				// console.log('what is you: ', typeof arrData); //array object of strings
-				res.send(arrData.map(JSON.parse));
-			});
-		});
+				 //array object of strings
+				console.log('worky: ', JSON.parse(arrData[0]).path);
+				let name = (string) => {
+					let stringArray = string.split('/');
+					let arrayLength = stringArray.length-1;
+					let name = string.split('/')[arrayLength];
+					let owner = string.split('/')[4];
+
+					return {name:name, owner:owner};
+				}
+
+				res.send(arrData.map( shit => {
+
+						let parsed = JSON.parse(shit);
+
+						parsed.tree.forEach(item => {
+							console.log('owner: ', item.path)
+							item.name = name(item.path).name;
+							//item.owner = name(item.url).owner;--errors in buquettes
+							// console.log('owner: ', item.owner)
+						});
+						console.log('is she parsed: ', parsed);
+						// console.log('string? ', typeof parsed);
+						return parsed;
+					})
+				);
+			})
+			.catch(err => {
+				console.log('.then with arrData: ', err);
+			})
+		})
+		.catch(err =>{
+        console.log('.then with body: ', err);
+      });
 	});
 
 
