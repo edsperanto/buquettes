6f20345ffb78de1aea6639b582b1acabbb892f20
Edward Gao <edsperanto@users.noreply.github.com>
2017-04-26 20:28:17 -1000

    added modified_at for trees and blobs

--- client/src/containers/FoldersContainer/index.js
+++ client/src/containers/FoldersContainer/index.js
@@ -6,30 +6,30 @@ import {updateBoxData} from '../../actions';
 
 class FoldersContainer extends Component {
   componentWillMount() {
-const xhr = new XMLHttpRequest();
-const genSearchableArr = (obj, dir) => {
-	Object.keys(obj).forEach(item => {
-		let searchEntry = {name: item, path: dir};
-		let {id, children} = obj[item];
-		searchEntry.id = id;
-		if(!!children) {
-			let newPath = dir + `/${item}`;
-			searchEntry.type = 'folder';
-			genSearchableArr(obj[item].children, newPath);
-		}else{
-			searchEntry.type = 'file';
+		const xhr = new XMLHttpRequest();
+		const genSearchableArr = (obj, dir) => {
+			Object.keys(obj).forEach(item => {
+				let searchEntry = {name: item, path: dir};
+				let {id, children} = obj[item];
+				searchEntry.id = id;
+				if(!!children) {
+					let newPath = dir + `/${item}`;
+					searchEntry.type = 'tree';
+					genSearchableArr(obj[item].children, newPath);
+				}else{
+					searchEntry.type = 'blob';
+				}
+				this.props.onUpdateBoxData(searchEntry);
+			});
 		}
-		this.props.onUpdateBoxData(searchEntry);
-	});
-}
-xhr.addEventListener('load', e => {
-	let {success, directory_structure} = JSON.parse(xhr.responseText);
-	if(success) {
-		let parsedArr = genSearchableArr(directory_structure, '/Box');
-	}
-});
-xhr.open('GET', '/oauth2/box/folders', true);
-xhr.send();
+		xhr.addEventListener('load', e => {
+			let {success, directory_structure} = JSON.parse(xhr.responseText);
+			if(success) {
+				let parsedArr = genSearchableArr(directory_structure, '/Box');
+			}
+		});
+		xhr.open('GET', '/oauth2/box/folders', true);
+		xhr.send();
   }
   render(){
     return (
