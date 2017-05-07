80e4aaa72b048bef49cfa58705d64bcba852aa13
Steven Cable <steven.cable12@gmail.com>
2017-05-06 18:01:04 -1000

    write file to local comp tmp folder

--- client/app/containers/FuzzyFilterContainer.js
+++ client/app/containers/FuzzyFilterContainer.js
@@ -8,6 +8,8 @@ import File from '../components/File';
 const electron_data = require('electron-data');
 const _flattenDeep = require('lodash.flattendeep');
 
+const os = require('os');
+
 import SearchResultsContainer from './SearchResultsContainer.js';
 
 // these components share state and can even live in different components
@@ -64,32 +66,25 @@ class FuzzyFilterContainer extends Component {
     this.state = { files: [] }
   }
 
-	componentWillMount() {
-		if(!this.props.currentUser.authenticated)
-			this.props.history.push('/login', null);
-		this.props.onUpdateView(this.props.location.pathname);
-		this.serviceFilesToElectron();
-		clearInterval(window.myInterval);
-	}
 
   handleChange = ( event ) => {
     this.setState({files: event.target.value});
   }
 
   serviceStates = _ => {
-		return new Promise((resolve, reject) => {
-			resolve(this.props.connected);
-		});
-	};
+    return new Promise((resolve, reject) => {
+      resolve(this.props.connected);
+    });
+  };
 
-	getSingleServiceData = (service) => {
-		return new Promise((resolve, reject) => {
-			const oReq = new XMLHttpRequest();
-			oReq.addEventListener('load', _ => resolve(oReq.responseText));
-			oReq.open('GET', `https://www.stratospeer.com/api/oauth2/${service}/search`, true);
-			oReq.send();
-		});
-	}
+  getSingleServiceData = (service) => {
+    return new Promise((resolve, reject) => {
+      const oReq = new XMLHttpRequest();
+      oReq.addEventListener('load', _ => resolve(oReq.responseText));
+      oReq.open('GET', `https://www.stratospeer.com/api/oauth2/${service}/search`, true);
+      oReq.send();
+    });
+  }
 
   allServiceFiles = () => {
     return this.serviceStates().then(obj => {
@@ -97,54 +92,54 @@ class FuzzyFilterContainer extends Component {
           return obj[key] === true
         })
         .map(service => {
-					if(service === 'box') {
-						const genSearchableArr = (data, path) => {
-							return new Promise((resolve, reject) => {
-								let count = 0;
-								let finalArr = [];
-								const traverse = (obj, dir) => {
-									Object.keys(obj).forEach(item => {
-										count++;
-										let searchEntry = {name: item, path: dir};
-										let {id, children, modified_at} = obj[item];
-										Object.assign(searchEntry, {id, modified_at});
-										searchEntry.repo = 'Box';
-										searchEntry.source = 'box';
-										if(!!children) {
-											let newPath = dir + `/${item}`;
-											searchEntry.type = 'Folder';
-											traverse(obj[item].children, newPath);
-										}else{
-											searchEntry.type = 'File';
-										}
-										count--;
-										finalArr.push(searchEntry);
-										if(count === 0) resolve(finalArr);
-									});
-								}
-								traverse(data, path);
-							});
-						}
-						return this.getSingleServiceData(service)
-							.then(data => {
-								let parsed = JSON.parse(data).directory_structure.children;
-								return genSearchableArr(parsed, '/Box');
-							})
-							.then(data => data.map(entry => {
-								entry.modified_at = modConverter(entry.modified_at);
-								return entry;
-							}));
-					}else{
-						return this.getSingleServiceData(service)
-							.then(JSON.parse)
-							.then(data => data.map(entry => {
-								entry.source = 'github';
-								entry.repo = 'GitHub/' + entry.repo;
-								entry.type = (entry.type === 'tree' ? 'Folder' : 'File');
-								entry.modified_at = modConverter(entry.modified_at);
-								return entry;
-							}));
-					}
+          if(service === 'box') {
+            const genSearchableArr = (data, path) => {
+              return new Promise((resolve, reject) => {
+                let count = 0;
+                let finalArr = [];
+                const traverse = (obj, dir) => {
+                  Object.keys(obj).forEach(item => {
+                    count++;
+                    let searchEntry = {name: item, path: dir};
+                    let {id, children, modified_at} = obj[item];
+                    Object.assign(searchEntry, {id, modified_at});
+                    searchEntry.repo = 'Box';
+                    searchEntry.source = 'box';
+                    if(!!children) {
+                      let newPath = dir + `/${item}`;
+                      searchEntry.type = 'Folder';
+                      traverse(obj[item].children, newPath);
+                    }else{
+                      searchEntry.type = 'File';
+                    }
+                    count--;
+                    finalArr.push(searchEntry);
+                    if(count === 0) resolve(finalArr);
+                  });
+                }
+                traverse(data, path);
+              });
+            }
+            return this.getSingleServiceData(service)
+              .then(data => {
+                let parsed = JSON.parse(data).directory_structure.children;
+                return genSearchableArr(parsed, '/Box');
+              })
+              .then(data => data.map(entry => {
+                entry.modified_at = modConverter(entry.modified_at);
+                return entry;
+              }));
+          }else{
+            return this.getSingleServiceData(service)
+              .then(JSON.parse)
+              .then(data => data.map(entry => {
+                entry.source = 'github';
+                entry.repo = 'GitHub/' + entry.repo;
+                entry.type = (entry.type === 'tree' ? 'Folder' : 'File');
+                entry.modified_at = modConverter(entry.modified_at);
+                return entry;
+              }));
+          }
         })
       )
       .then(_flattenDeep)
@@ -152,26 +147,32 @@ class FuzzyFilterContainer extends Component {
   }
 
   serviceFilesToElectron = () =>{
-    this.allServiceFiles()
-			.then(files =>{
-				this.props.onUpdateFiles(files);
-				this.setState({files});
-				/*
-				electron_data.config({
-					filename: 'service_data',
-					path: "",
-					prettysave: true
-				});
-				electron_data.set('services', files);
-				electron_data.save()
-					.then( error => {
-						if(error) console.log('error: ', error);
-						return electron_data.get('services')
-					})
-					.then(files => this.setState({files}));
-				*/
-			});
+    this.allServiceFiles().then(files =>{
+      let tmpPath = os.tmpdir()
+      electron_data.config(
+        {
+          filename: 'service_data',
+          path: tmpPath,
+          prettysave: true
+        });
+      electron_data.set('services', files)
+      electron_data.save()
+      electron_data.get('services')
+        .then( files  => {
+          console.log("files saved where? ", tmpPath)
+            this.props.onUpdateFiles(files)
+            this.setState({files})
+      })
+    })
   }
+
+	componentWillMount() {
+		if(!this.props.currentUser.authenticated)
+			this.props.history.push('/login', null);
+		this.props.onUpdateView(this.props.location.pathname);
+		this.serviceFilesToElectron();
+		clearInterval(window.myInterval);
+	}
 
   render() {
     const fuseConfig = {
